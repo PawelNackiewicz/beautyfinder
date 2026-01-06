6e077c202a37010d86b52f1128752b21c3a7af20
Pawel Nackiewicz <it.pnackiewicz@gmail.com>
2026-01-06 21:02:19 +0100

    check env var

--- apps/web-manager/src/features/landing/components/Footer.tsx
+++ apps/web-manager/src/features/landing/components/Footer.tsx
@@ -74,6 +74,9 @@ export const Footer = () => {
             >
               Polityka prywatności
             </a>
+            <p>
+              {process.env.WEB_MANAGER_APP_URL ?? "Failed env variable"}
+            </p>
             <a
               href="#"
               className="text-sm text-muted-foreground hover:text-primary transition-colors"
